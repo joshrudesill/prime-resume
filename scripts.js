ac126204bb79d5ee9233962055552f5d2cfeb260
Joshua Rudesill <joshrudesill@gmail.com>
2023-08-31 20:03:08 -0500

    skills section started

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
-  load();
-});
+window.addEventListener("DOMContentLoaded", () => {});
 
 function openLinkedIn() {
   window.open("https://www.linkedin.com/in/josh-rudesill/", "_blank").focus();
